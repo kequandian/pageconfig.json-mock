2b0175d74276e5d281b4771ddb0933db8378dc8a
zchengb <zxchengb@163.com>
2021-05-19 14:33:45 +0800

    Fixed network issue about loopback address in docker.

--- src/index.js
+++ src/index.js
@@ -61,5 +61,5 @@ low(adapter)
         return db.defaults({ posts: [] }).write()
     })
     .then(() => {
-        app.listen(3000, () => console.log('listening on port 3000'))
+        app.listen(3000, '0.0.0.0', () => console.log('listening on port 3000'))
     })
